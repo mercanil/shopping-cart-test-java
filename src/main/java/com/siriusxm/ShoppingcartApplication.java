a66a907e0e85d918ead2576b722f8ecc16f9b54d
anil mercan <mercanil@gmail.com>
2025-10-02 21:45:16 +0100

    delete unnecessary spring boot test

--- src/main/java/com/siriusxm/ShoppingcartApplication.java
+++ src/main/java/com/siriusxm/ShoppingcartApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingcartApplication {
 
-	public static void main(String[] args) {
-        System.out.println("Good luck on the take home!");
-	}
+    public static void main(String[] args) {
+    }
 
 }
